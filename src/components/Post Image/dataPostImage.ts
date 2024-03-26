13baa982244faa521759764bcbaf0662bfd7a1d5
alejahurtd <mairahurtado930@gmail.com>
2024-03-25 22:05:53 -0500

    Docs: The data was added and the structure of the objects was defined.

---
+++ src/components/Post Image/dataPostImage.ts
@@ -0,0 +1,43 @@
+export interface postImage {
+	image: string;
+	isLiked: boolean;
+	isSaved: boolean;
+	likesCount: number;
+	username: string;
+	description: string;
+}
+
+export const dataPostImage = [
+	{
+		image: 'https://i.pinimg.com/564x/08/09/5a/08095a891a2c668646b10975e6ab561e.jpg',
+		isLiked: true,
+		isSaved: false,
+		likesCount: 950,
+		username: '@val0931',
+		description: 'I gave it all',
+	},
+	{
+		image: 'https://i.pinimg.com/564x/08/09/5a/08095a891a2c668646b10975e6ab561e.jpg',
+		isLiked: true,
+		isSaved: false,
+		likesCount: 590,
+		username: '@alejah__',
+		description: 'Sweat today, smile tomorrow',
+	},
+	{
+		image: 'https://i.pinimg.com/564x/08/09/5a/08095a891a2c668646b10975e6ab561e.jpg',
+		isLiked: true,
+		isSaved: false,
+		likesCount: 1300,
+		username: '@guti_santi',
+		description: 'Sweat, smile, repeat.',
+	},
+	{
+		image: 'https://i.pinimg.com/564x/08/09/5a/08095a891a2c668646b10975e6ab561e.jpg',
+		isLiked: true,
+		isSaved: false,
+		likesCount: 480,
+		username: '@juanesv08',
+		description: 'Exercise: self-love in motion.',
+	},
+];
